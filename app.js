64247556909f927bb6304eaa6b4c24562a8f83c5
oiclai <claradealcantara285@gmail.com>
2024-12-31 16:51:20 -0300

    :bulb: docs: comment code

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-function carregarHorarios() {
-    fetch('horarios.json')
-        .then(response => response.json()) 
-        .then(data => {
-            exibirHorarios(data);
-        })
-        .catch(err => console.error('Erro ao carregar os horários:', err));
-}
-function exibirHorarios(horariosMissas) {
-    const tabela = document.getElementById('tabela-missas');
-    tabela.innerHTML = '';
+// function carregarHorarios() {
+//     fetch('horarios.json')
+//         .then(response => response.json()) 
+//         .then(data => {
+//             exibirHorarios(data);
+//         })
+//         .catch(err => console.error('Erro ao carregar os horários:', err));
+// }
+// function exibirHorarios(horariosMissas) {
+//     const tabela = document.getElementById('tabela-missas');
+//     tabela.innerHTML = '';
 
-    horariosMissas.forEach(missa => {
-        const linha = document.createElement('tr');
-        linha.innerHTML = `
-            <td>${missa.igreja}</td>
-            <td>${missa.bairro}</td>
-            <td>${missa.horario}</td>
-            <td>${missa.turno}</td>
-            <td>${missa.dia}</td>
-        `;
-        tabela.appendChild(linha);
-    });
-}
+//     horariosMissas.forEach(missa => {
+//         const linha = document.createElement('tr');
+//         linha.innerHTML = `
+//             <td>${missa.igreja}</td>
+//             <td>${missa.bairro}</td>
+//             <td>${missa.horario}</td>
+//             <td>${missa.turno}</td>
+//             <td>${missa.dia}</td>
+//         `;
+//         tabela.appendChild(linha);
+//     });
+// }
 
-document.addEventListener('DOMContentLoaded', carregarHorarios);
+// document.addEventListener('DOMContentLoaded', carregarHorarios);

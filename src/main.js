5f134c535468f0fa4f4c0f854430ca60f0cb9fdd
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2025-01-27 15:40:39 -0300

    :bug: fix: fix bugs

--- src/main.js
+++ src/main.js
@@ -2,31 +2,35 @@ import missas from "./pages/array.js";
 
 
 document.getElementById("oeste").addEventListener("click", () => {
-    let missasOeste = filterMissasByZona("zona oeste");
+    let missasOeste = filterMissasByZona("Zona Oeste");
+    renderModal(renderMissas(missasOeste))
 });
 document.getElementById("sul").addEventListener("click", () => {
-    let missasSul = filterMissasByZona("zona sul");
+    let missasSul = filterMissasByZona("Zona Sul");
+    renderModal(renderMissas(missasSul))
 });
 
-document.getElementById("leste").addEventListener("click", () => {
-    let missasLeste = filterMissasByZona("zona leste");
+document.getElementById("leste").addEventListener("click", () => { // presta
+    let missasLeste = filterMissasByZona("Zona Leste");
+    renderModal(renderMissas(missasLeste))
 });
 
 document.getElementById("norte").addEventListener("click", () => {
-    let missasNorte = filterMissasByZona("zona norte");
+    let missasNorte = filterMissasByZona("Zona Norte");
+    renderModal(renderMissas(missasNorte))
 });
 
 function filterMissasByZona(zona) {
     return missas.filter((missa) => missa.zona === zona);
 }
-
+// modal
 function renderModal(content) {
     const modal = document.createElement("div");
-    modal.className= "fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50";
+    modal.className= "fixed inset-0 bg-pink bg-opacity-10 flex items-center justify-center z-50";
     modal.innerHTML = `
     <div class= "max-h-[600px] overflow-auto relative border rounded-lg bg-neutral-200 p-1">
-        <button class="sticky top-3 text-neutral-900" id="closeButton">
-        aa
+        <button class="sticky top-3 text-pink-900" id="closeButton">
+        X
         </button>
         ${content}
     </div>
@@ -37,12 +41,14 @@ function renderModal(content) {
     document.getElementById("closeButton").addEventListener("click", () => {
         document.body.removeChild(modal);
 });
+
+
 }
 function renderMissas(missas) {
     let list = missas
         .map(
             (missa) => `
-    <div class="missa border p-4 rounded bg-gray-100">
+    <div class="missa border p-4 rounded bg-blue-100">
       <h3 class="font-bold text-lg">${missa.igreja}</h3>
       <p><strong>Horário:</strong> ${missa.horario}</p>
       <p><strong>Bairro:</strong> ${missa.bairro}</p>

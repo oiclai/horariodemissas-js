ef96e8a8a85b6cd7e34b633a5fffcd1e56452727
Clara Almeida <gomes.almeida@academico.ifpb.edu.br>
2025-01-27 22:24:26 -0300

    :lipstick: style: page

--- src/main.js
+++ src/main.js
@@ -26,11 +26,11 @@ function filterMissasByZona(zona) {
 function renderModal(content) {
     const modal = document.createElement("div");
     modal.className =
-        "fixed top-0 right-0 h-screen w-[550px] bg-white shadow-lg z-50 overflow-auto";
+        "fixed top-0 right-0 h-screen w-[550px] shadow-lg z-50 overflow-auto";
     modal.innerHTML = `
     <div class= "max-h-[600px] overflow-auto relative border rounded-lg bg-neutral-200 p-1">
         <button class="absolute top-2 right-2 text-[#8C5383] font-bold" id="closeButton">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="m7 7l10 10M7 17L17 7"/></svg>
+         x
         </button>
         ${content}
     </div>
@@ -46,7 +46,7 @@ function renderMissas(missas) {
     let list = missas
         .map(
             (missa) => `
-    <div class="text-[#8C5383] missa border p-4 rounded bg-[#D7E7B3]">
+    <div class="text-[#9C6351] missa border p-4 rounded bg-[#E6DFCC]">
       <h3 class="font-bold text-lg">${missa.igreja}</h3>
       <p><strong>Horário:</strong> ${missa.horario}</p>
       <p><strong>Bairro:</strong> ${missa.bairro}</p>
